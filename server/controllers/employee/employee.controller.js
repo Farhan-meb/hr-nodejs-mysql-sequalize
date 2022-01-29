40c75ebba2f71e07ae45903e25784c8e721c0a7a
Farhaan mahbub <farhan.meb@gmail.com>
2022-01-29 18:11:01 +0600

    updated

--- server/controllers/employee/employee.controller.js
+++ server/controllers/employee/employee.controller.js
@@ -1,13 +1,12 @@
-const db = require("../../database");
+const db = require('../../database');
 const Employee = db.employees;
 const Op = db.Sequelize.Op;
 const fs = require('fs');
 const csv = require('csv-parser');
 const catchAsync = require('../../utils/catchAsync');
 const appError = require('../../utils/appError');
-const AppError = require("../../utils/appError");
+const AppError = require('../../utils/appError');
 const email = require('../../helpers/email');
-
 
 const addEmployee = catchAsync(async (req, res, next) => {
     const { first_name, last_name, email } = req.body;
@@ -19,55 +18,54 @@ const addEmployee = catchAsync(async (req, res, next) => {
     const employee = {
         first_name,
         last_name,
-        email
+        email,
     };
 
     await Employee.create(employee)
-        .then(data => {
+        .then((data) => {
             res.status(201).json({
                 message: 'Successfully created employee!',
                 employee: data,
-            })
+            });
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).json({
                 message:
-                    err.message || "Some error occurred while creating employee."
+                    err.message ||
+                    'Some error occurred while creating employee.',
             });
         });
-
 });
 
 const getAllEmployees = catchAsync(async (req, res, next) => {
     const { email } = req.body;
 
-    const limit = parseInt(req.query.limit), page = parseInt(req.query.page);
+    const limit = parseInt(req.query.limit) || 5,
+        page = parseInt(req.query.page) || 1;
 
     //if needed to search employees by email in future
     const condition = email ? { email: { [Op.like]: `%${email}%` } } : null;
-
 
     await Employee.findAll({
         limit: limit,
         offset: (page - 1) * limit,
         where: condition,
-        order: [
-            ['createdAt', 'DESC']
-        ],
-        attributes: ['id', 'first_name', 'last_name', 'email']
+        order: [['createdAt', 'DESC']],
+        attributes: ['id', 'first_name', 'last_name', 'email'],
     })
-        .then(data => {
+        .then((data) => {
             res.status(200).json({
                 employees: data,
-            })
+            });
         })
-        .catch(err => {
+        .catch((err) => {
             res.status(500).send({
                 message:
-                    err.message || "Some error occurred while retrieving employees."
+                    err.message ||
+                    'Some error occurred while retrieving employees.',
             });
         });
-})
+});
 
 const sendEmail = catchAsync(async (req, res, next) => {
     const { emails, subject, body } = req.body;
@@ -80,66 +78,71 @@ const sendEmail = catchAsync(async (req, res, next) => {
 
     if (response) {
         res.status(200).json({
-            message: "Email sent successfully to : " + emails,
-        })
-    }
-    else {
+            message: 'Email sent successfully to : ' + emails,
+        });
+    } else {
         res.status(200).json({
-            message: "Couldnt send email!"
-        })
+            message: 'Couldnt send email!',
+        });
     }
-
-})
+});
 
 const importBulkEmployees = catchAsync(async (req, res, next) => {
-
     console.log('duksi');
 
     const file = req.file;
 
-    let added = 0, skipped = 0;
+    let added = 0,
+        skipped = 0;
 
-    let addedEmployees = [], failedEmployees = [];
+    let addedEmployees = [],
+        failedEmployees = [];
 
     fs.createReadStream('./uploads/' + file.filename)
         .pipe(csv({}))
         .on('data', async (data) => {
-            let totalLength = data && Object.values(data).length;
             const { first_name, last_name, email } = data;
 
             if (!first_name || !last_name || !email) {
                 skipped += 1;
-            }
-            else {
+            } else {
                 const employee = {
                     first_name,
                     last_name,
-                    email
+                    email,
                 };
 
                 await Employee.create(employee)
-                    .then(data => {
+                    .then((data) => {
                         added += 1;
                         console.log(added);
                         addedEmployees.push(data);
                     })
-                    .catch(err => {
+                    .catch((err) => {
                         skipped += 1;
                         failedEmployees.push(employee);
                     });
             }
-
         })
         .on('end', () => {
             console.log('end');
             res.status(201).json({
-                message: 'Employees successfully added : ' + added + '\n Employees failed adding : ' + skipped,
+                message:
+                    'Employees successfully added : ' +
+                    added +
+                    '\n Employees failed adding : ' +
+                    skipped,
                 addedEmployees,
                 failedEmployees,
-            })
+            });
         });
 
     console.log('added!', added);
-})
+});
 
-module.exports = { addEmployee, getAllEmployees, sendEmail, importBulkEmployees }
+module.exports = {
+    addEmployee,
+    getAllEmployees,
+    sendEmail,
+    importBulkEmployees,
+};
--- server/models/employee/employee.model.js
+++ server/models/employee/employee.model.js
@@ -1,29 +1,42 @@
 module.exports = (sequelize, DataTypes) => {
-  const Employee = sequelize.define("employee", {
-    first_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: { arg: [2, 30], msg: 'First name must contain 2-30 characters!' }
-      }
-    },
-    last_name: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: { arg: [2, 30], msg: 'First name must contain 2-30 characters!' }
-      }
-    },
-    email: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        len: [2, 40],
-        isEmail: { arg: true, msg: 'Please provide a valid email!' }
-      }
-    }
+    const Employee = sequelize.define('employee', {
+        first_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [2, 32],
+                isAlpha: {
+                    arg: true,
+                    msg: 'Please provide a valid first name!',
+                },
+            },
+        },
+        last_name: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                max: {
+                    args: [30],
+                    msg: 'Maximum 30 characters allowed in last name',
+                },
+                min: {
+                    args: [4],
+                    msg: 'Minimum 4 characters required in last name',
+                },
+                isAlpha: {
+                    arg: true,
+                    msg: 'Please provide a valid last name!',
+                },
+            },
+        },
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isEmail: { arg: true, msg: 'Please provide a valid email!' },
+            },
+        },
+    });
 
-  });
-
-  return Employee;
+    return Employee;
 };
