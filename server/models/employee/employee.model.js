7ef41f737d27665ca1260ee6f7a2f4625ecdce75
Farhaan mahbub <farhan.meb@gmail.com>
2022-01-31 17:18:10 +0600

    Update employee.model.js

--- server/models/employee/employee.model.js
+++ server/models/employee/employee.model.js
@@ -1,50 +1,63 @@
 module.exports = (sequelize, DataTypes) => {
-    const Employee = sequelize.define('employee', {
-        first_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validate: {
+    const Employee = sequelize.define(
+        'employee',
+        {
+            first_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    min: {
+                        args: [4],
+                        msg: 'Minimum 4 characters required in fist name',
+                    },
+                    max: {
+                        args: [30],
+                        msg: 'Maximum 30 characters allowed in first name',
+                    },
+                    isAlpha: {
+                        arg: true,
+                        msg: 'First name must contain only alphabetic character!',
+                    },
+                },
+            },
+            last_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
                 min: {
                     args: [4],
-                    msg: 'Minimum 4 characters required in fist name',
+                    msg: 'Minimum 4 characters required in last name',
                 },
                 max: {
                     args: [30],
-                    msg: 'Maximum 30 characters allowed in first name',
+                    msg: 'Maximum 30 characters allowed in last name',
                 },
-                isAlpha: {
-                    arg: true,
-                    msg: 'First name must contain only alphabetic character!',
+                validate: {
+                    isAlpha: {
+                        arg: true,
+                        msg: 'First name must contain only alphabetic character!',
+                    },
                 },
             },
-        },
-        last_name: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            min: {
-                args: [4],
-                msg: 'Minimum 4 characters required in last name',
-            },
-            max: {
-                args: [30],
-                msg: 'Maximum 30 characters allowed in last name',
-            },
-            validate: {
-                isAlpha: {
-                    arg: true,
-                    msg: 'First name must contain only alphabetic character!',
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    isEmail: {
+                        arg: true,
+                        msg: 'Please provide a valid email!',
+                    },
                 },
             },
         },
-        email: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true,
-            validate: {
-                isEmail: { arg: true, msg: 'Please provide a valid email!' },
-            },
-        },
-    });
+        {
+            indexes: [
+                {
+                    unique: true,
+                    fields: ['email'],
+                },
+            ],
+        }
+    );
 
     return Employee;
 };
